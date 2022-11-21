951bc9819eab2da860b69a62c328aac4a44df598
Petrus Pierre <petrus.tsx@gmail.com>
2022-11-20 21:51:24 -0300

    add colors documentation

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,25 @@
+import { colors } from '@oxe-ui/tokens'
+import { getContrast } from 'polished'
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color:
+              getContrast(color, colors.white) < 3.5
+                ? colors.black
+                : colors.white,
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
